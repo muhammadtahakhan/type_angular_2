a67206e3d02003423bbe83f3f34afb7e0fe75343
Taha <muhammadtahakhan222@gmail.com>
2016-03-20 23:53:27 +0500

    workin with componnt directive services DI

---
+++ angular2-seed/app/authors.component.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core'
+import {AuthorService} from './author.service'
+@Component({
+    selector:'authors',
+    template:`
+    <h2>Authors</h2>
+    <ul>
+    <li *ngFor="#author of authors">{{author}}</li>
+    </ul>
+    `,
+    providers:[AuthorService]
+})
+export class AuthorComponent{
+    authors:string[];
+    title="Title for autor page";
+    constructor(authorService:AuthorService){
+        this.authors = authorService.getAuthors();
+    }
+}
