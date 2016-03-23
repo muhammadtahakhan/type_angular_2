ec3268bb567143caad99c0e6e94d6c8f1a43cee1
Taha <muhammadtahakhan222@gmail.com>
2016-03-24 00:34:20 +0500

    obserable working

---
+++ services-1/nodejs-jwt-authentication-sample/anonymous-routes.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+    quoter  = require('./quoter');
+
+var app = module.exports = express.Router();
+
+app.get('/api/random-quote', function(req, res) {
+//   res.status(200).send(quoter.getRandomOne());
+res.status(200).send(quoter.getRandomOne());
+});
