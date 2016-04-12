7cc76a7dddc7fe8680b2a1051bfa75ff078a7da3
Taha <muhammadtahakhan222@gmail.com>
2016-04-12 17:23:45 +0500

    node API for jsonwebtoken

--- REST/routes/api.js
+++ REST/routes/api.js
@@ -1,57 +1,168 @@
 var express = require('express');
-var router = express.Router();
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./../config'); // get our config file
+var User   = require('./../models/user'); // get our mongoose model
+var config = require('./../config');
+var apiRoutes = express.Router();     
 
-//Models
-var Product = require('../models/product');
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, config.secret, function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
 
 
-router.get('/', function(req, res) {
-//   res.send("API");
-   Product.find(function (err, todos) {
-    if (err) return next(err);
-    res.json(todos);
-  });
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
 
- });
+// route to return all users (GET http://localhost:8080/api/users)
+ 
+apiRoutes.post('/users', function(req, res) {
+  User.findOne({
+      name: req.body.name
+    }, function(err, user) {
+        if (err) throw err;
+ 
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+        } else {
+          res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+        }
+    });
+});  
 
 
-router.post('/', function(req, res) {
-//   res.send("API");
-  Product.create(req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+      name: req.body.name
+    }, function(err, user) {
+        if (err) throw err;
+  
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. User not found..'});
+        } else if (user) {
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+            var token = jwt.sign(user, config.secret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
 
- });
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+        }
+    });
+});  
+
+module.exports = apiRoutes; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var express = require('express');
+// var router = express.Router();
+
+// //Models
+// var Product = require('../models/product');
+
+
+
+// router.get('/', function(req, res) {
+// //   res.send("API");
+//    Product.find(function (err, todos) {
+//     if (err) return next(err);
+//     res.json(todos);
+//   });
+
+//  });
+
+
+// router.post('/', function(req, res) {
+// //   res.send("API");
+//   Product.create(req.body, function (err, post) {
+//     if (err) return next(err);
+//     res.json(post);
+//   });
+
+//  });
  
  
-router.put('/:id', function(req, res) {
-//   res.send("API");
- Product.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
- });
+// router.put('/:id', function(req, res) {
+// //   res.send("API");
+//  Product.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+//     if (err) return next(err);
+//     res.json(post);
+//   });
+//  });
 
-router.delete('/:id', function(req, res) {
-//   res.send("API");
- Product.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+// router.delete('/:id', function(req, res) {
+// //   res.send("API");
+//  Product.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+//     if (err) return next(err);
+//     res.json(post);
+//   });
 
- });
-
-
-
-//roures
-// Product.methods(['get', 'put', 'push', 'delete']);
-// Product.register(router, '/products')
+//  });
 
 
-// router.get('/products', function(req, res){
-//     res.send('api is working');
-// });
 
-//return router
-module.exports = router; 
+// //roures
+// // Product.methods(['get', 'put', 'push', 'delete']);
+// // Product.register(router, '/products')
+
+
+// // router.get('/products', function(req, res){
+// //     res.send('api is working');
+// // });
+
+// //return router
+// module.exports = router; 
--- REST/server.js
+++ REST/server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var product = require('./routes/api')
+var apiRoutes = require('./routes/api');
 
-//MongoDB
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User   = require('./models/user'); // get our mongoose model
+    
+
+var port = process.env.PORT || 8080;
+//MongoDB 
+// app.set('superSecret', config.secret);
 mongoose.connect('mongodb://localhost:27017/rest_test', function(err) {
     if(err) {
         console.log('connection error', err);
@@ -12,15 +19,88 @@ mongoose.connect('mongodb://localhost:27017/rest_test', function(err) {
     }
 });
 
+
+
 //Express
 var app = express();
-app.use(bodyParser.urlencoded({ extended:true}));
-app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
 
-//Routes
-app.use('/api',product);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:'+port+'/api');
+});
+//return user jsonwebaccesstoken
+app.post('/authenticate', function(req, res) {
+    // console.log(req);
+    console.log(req.body.name);
+  User.findOne({
+      name: req.body.name
+    }, function(err, user) {
+        if (err) throw err;
+  
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. User not found..'});
+        } else if (user) {
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+            var token = jwt.sign(user, config.secret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+        }
+    });
+});  
 
 
-//Starting server
-app.listen(3000);
-console.log("API is runnig on port 3000");
+
+
+
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({ 
+    name: 'name', 
+    password: 'password',
+    admin: true 
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+// route to a user (POST http://localhost:8080/api/authenticate)
+app.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+}); 
+
+
+app.use('/api', apiRoutes);
+
+app.listen(port);
+console.log("API is runnig on port "+port);
